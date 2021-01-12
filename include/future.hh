4aca03f480bb9bf1c7cfba27dfaa389fa0b05950
Karol Baryła <lorak.mmk@gmail.com>
2021-01-13 00:08:39 +0100

    Added batch & async queries

---
+++ include/future.hh
@@ -0,0 +1,29 @@
+#pragma once
+#include "cassandra.h"
+#include "prepared_query.hh"
+#include "query_result.hh"
+
+namespace scmd {
+class future {
+    CassFuture *_future;
+public:
+    explicit future(CassFuture *future);
+
+    // We can't really copy this class
+    future(const future &other) = delete;
+    future &operator=(const future &other) = delete;
+
+    future(future &&other) noexcept;
+    future &operator=(future &&other) noexcept;
+
+    ~future();
+
+    bool is_ready();
+
+    void wait();
+
+    query_result get_result();
+
+    prepared_query get_prepared();
+};
+}  // namespace scmd
---
+++ src/future.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+
+
+#include "exceptions.hh"
+#include "future.hh"
+#include "prepared_query.hh"
+
+scmd::future::future(CassFuture *future) {
+    this->_future = future;
+}
+
+scmd::future::future(scmd::future &&other) noexcept
+    : _future(std::exchange(other._future, nullptr)) {}
+
+scmd::future &scmd::future::operator=(scmd::future &&other) noexcept {
+    std::swap(other._future, this->_future);
+    return *this;
+}
+
+scmd::future::~future() {
+    cass_future_free(this->_future);
+}
+
+bool scmd::future::is_ready() {
+    return cass_future_ready(this->_future) == cass_true;
+}
+
+void scmd::future::wait() {
+    cass_future_wait(this->_future);
+}
+
+scmd::query_result scmd::future::get_result() {
+    const CassResult *result = cass_future_get_result(this->_future);
+    if(result == nullptr) {
+    //TODO: Maybe throw error result and/or error message?
+        scmd_internal::throw_on_cass_error(cass_future_error_code(this->_future));
+    }
+    return scmd::query_result(result);
+}
+
+scmd::prepared_query scmd::future::get_prepared() {
+    const CassPrepared *result = cass_future_get_prepared(this->_future);
+    if(result == nullptr) {
+        //TODO: Maybe throw error result and/or error message?
+        scmd_internal::throw_on_cass_error(cass_future_error_code(this->_future));
+    }
+    return scmd::prepared_query(result);
+}
