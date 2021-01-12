a4efe41cc9a567b73f05f285f96c88507873106e
Karol Baryła <lorak.mmk@gmail.com>
2021-01-12 19:44:12 +0100

    Removed code that was using fmt. Created convenience header for end users

---
+++ include/scmd.hh
@@ -0,0 +1,7 @@
+#pragma once
+
+// Heler include file, so library users can just include it, and no other individual files.
+#include "prepared_query.hh"
+#include "query_result.hh"
+#include "session.hh"
+#include "statement.hh"
